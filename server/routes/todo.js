32beba09d236cff0c44c2d4a59b84fc99a13b4e4
Chirag-Punia <cpunia_be21@thapar.edu>
2024-05-06 12:59:54 +0530

    ##

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -16,8 +16,8 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   auth: {
-    user: process.env.xemail,
-    pass: process.env.pass,
+    user: process.env.XEMAIL,
+    pass: process.env.PASS,
   },
 });
 const handleJob = async (date, userID, task, description) => {
